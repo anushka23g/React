a4cd229edbe32988014acdcf06ab3b8a84ca31d4
Anushka Gupta <42449841+anushka23g@users.noreply.github.com>
2021-01-06 23:41:48 +0530

    variables

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import reactDOM from "react-dom";
 
 var fname = "Tom";
 var lname = "Sawyer";
+var c= "V";
 reactDOM.render(
   <div>
-    <h1>Hi, my first name is {fname}</h1>
-    <h1>and my last name is {lname}</h1>
+    <h1>Hi, my first name is {fname}.</h1>
+    <h1>My last name is {lname}.</h1>
+    <h1>I study in {c}th standard.</h1>
   </div>,
   document.getElementById("root")
 );
